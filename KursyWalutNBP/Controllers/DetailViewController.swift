ccf75e15cb97625933dfbca03fd7940aed6bb052
s426242 <tymek.pawel@gmail.com>
2020-01-14 18:29:40 +0100

    Added date pickers

--- KursyWalutNBP/Controllers/DetailViewController.swift
+++ KursyWalutNBP/Controllers/DetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  CurrencyListViewController.swift
+//  DetailViewController.swift
 //  KursyWalutNBP
 //
 //  Created by xxx on 12/01/2020.
@@ -22,24 +22,31 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
     
         //
-        // MARK: - Custom Navigation Bar
+        // MARK: - Custom Navigation Controller look
         //
+        
+        // Custom Navigation Bar
         navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         
-        //
-        // MARK: - Custom Tab Bar
-        //
+        // Custom Tab Bar
         tabBarController?.tabBar.barTintColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         
         // first load data
         fetchCurrenyData()
         
-//        setChartView()
         
-        //
-        // MARK: - Refresh Table View
-        //
+        // Refresh Table View
         tableView.refreshControl = refresher
+        
+        startDatePicker = UIDatePicker()
+        startDatePicker?.datePickerMode = .date
+        startDateTextField.inputView = startDatePicker
+        
+        endDatePicker = UIDatePicker()
+        endDatePicker?.datePickerMode = .date
+        endDateTextField.inputView = endDatePicker
+        
+        
     }
     
     //
@@ -50,6 +57,9 @@ class DetailViewController: UIViewController {
     var name: String = ""
     var code: String = ""
     var tableName: String = ""
+    var i = 0
+    var startDatePicker: UIDatePicker?
+    var endDatePicker: UIDatePicker?
     
     lazy var refresher: UIRefreshControl = {
         let refreshControl = UIRefreshControl()
@@ -59,13 +69,14 @@ class DetailViewController: UIViewController {
         return refreshControl
     }()
     
-    var i = 0
  
     //
     // MARK: - Outlets
     //
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var lineChartView: LineChartView!
+    @IBOutlet weak var startDateTextField: UITextField!
+    @IBOutlet weak var endDateTextField: UITextField!
     
     //
     // MARK: - available to Objective-C
@@ -80,17 +91,19 @@ class DetailViewController: UIViewController {
     }
     
     
+    // Present chart
     func setChartView(chartData: [ChartDataEntry]) {
         let dataSet = LineChartDataSet(values:chartData,label:code)
         let data = LineChartData(dataSet:dataSet)
         self.lineChartView.data = data
-            
-            lineChartView.data?.setValueTextColor(NSUIColor.white)
-            lineChartView.data?.setValueFont(NSUIFont.systemFont(ofSize: 0))
-            lineChartView.xAxis.labelTextColor = UIColor.white
-            lineChartView.leftAxis.labelTextColor = UIColor.white
-            lineChartView.rightAxis.labelTextColor = UIColor.white
-            lineChartView.legend.textColor = UIColor.white
+           
+        // Chart appearance
+        lineChartView.data?.setValueTextColor(NSUIColor.white)
+        lineChartView.data?.setValueFont(NSUIFont.systemFont(ofSize: 0))
+        lineChartView.xAxis.labelTextColor = UIColor.white
+        lineChartView.leftAxis.labelTextColor = UIColor.white
+        lineChartView.rightAxis.labelTextColor = UIColor.white
+        lineChartView.legend.textColor = UIColor.white
         }
     
     
@@ -103,22 +116,36 @@ class DetailViewController: UIViewController {
         Alamofire.request(apiUrl, method: .get).responseJSON(completionHandler: { (response) in
             switch response.result {
             case .success(let value):
+                
+                // Clear data
                 self.currencies.removeAll()
                 self.chartData.removeAll()
+                
                 self.i = 0
+                
+                // Parse json data
                 let data = JSON(value)
                 data["rates"].array?.forEach({(currency) in
+                    // Creating Currency object
                     let currency = Currency(tableName: self.tableName, currencyName: self.name, currencyCode: self.code, averageCurrencyRate: currency["mid"].doubleValue, effectiveDate: currency["effectiveDate"].stringValue)
                     self.currencies.append(currency)
+                    
+                    //
+                    // MARK: - Set values for Chart
+                    //
+                    
+                    // Set value to chart point
                     let point = ChartDataEntry(x: Double(self.i), y: currency.averageCurrencyRate)
                     self.chartData.append(point)
                     self.i = self.i + 1
                     })
-                
+                    // Present chart
+                    self.setChartView(chartData: self.chartData)
                     
-                self.setChartView(chartData: self.chartData)
+                    // Reload table view data
                     self.tableView.reloadData()
                     print("table reloaded")
+                
                 case .failure(let error):
                     print(error.localizedDescription)
                 }
